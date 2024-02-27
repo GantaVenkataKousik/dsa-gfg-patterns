ccda0cf26628c4368a6014f2c8823bb7f789781f
gantavenkatakousik <venkatakousikcse01@gmail.com>
2024-02-27 13:39:18 +0530

    Create Play With OR.java

---
+++ POTD/Easy/Play With OR.java
@@ -0,0 +1,14 @@
+class Solution{
+    
+   
+    // Function for finding maximum and value pair
+    public static int[] game_with_number (int arr[], int n) {
+
+        for(int i=0 ; i<n-1 ; i++){
+            arr[i] = (arr[i] | arr[i+1]);
+        }
+        return arr;
+    }
+    
+    
+}
