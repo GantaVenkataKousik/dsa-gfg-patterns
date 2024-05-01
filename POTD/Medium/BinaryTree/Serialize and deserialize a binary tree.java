4889e0c52b0c097f50493ef020849baa62eaa861
gantavenkatakousik <venkatakousikcse01@gmail.com>
2024-05-02 00:17:39 +0530

    Create Serialize and deserialize a binary tree.java

---
+++ POTD/Medium/BinaryTree/Serialize and deserialize a binary tree.java
@@ -0,0 +1,36 @@
+class Tree {
+    // Function to serialize a tree and return a list containing nodes of tree.
+    public void ser(ArrayList<Integer> arr , Node root){
+        if(root==null){
+            arr.add(-1);
+            return ;
+        }
+        arr.add(root.data);
+        ser(arr,root.left);
+        ser(arr,root.right);
+    }
+    
+    public ArrayList<Integer> serialize(Node root) {
+       ArrayList<Integer> arr = new ArrayList<>();
+       ser(arr,root);
+       return arr;
+    }
+
+
+    int index = -1;
+    public Node deSerialize(ArrayList<Integer> A) {
+        index = -1;
+        return des(A);
+    }
+    public Node des(ArrayList<Integer> a){
+        index++;
+        if(a.get(index)==-1)return null;
+        Node node = new Node(a.get(index));
+        
+        node.left = des(a);
+        node.right = des(a);
+        
+        return node;
+    }
+
+};
